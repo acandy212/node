2d9884fe5b254d1277ea0548ff60cc1a3f78e303
acandy212 <candies_amanda@columbusstate.edu>
2022-10-13 23:16:23 +0000

    added mongoose

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 var express = require('express');
+var mongoose = require('mongoose')
 var app = express();
 
+
 app.use('/static', express.static("public"));
+app.use(express.urlencoded({ extended: true }))
 app.set("view engine", "ejs")
+
+const mongoDB = 'mongodb+srv://candies_amanda:Ndx2dZV7I5rUTaJv@cluster0.godqah8.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console,"MongoDB connection error: "))
+
 
 app.get('/', function(req, res){
     res.render('todo.ejs');
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body.content)
 })
 
 app.listen(3000, function(){
